912056920c9fefb3ca282d24397e26eee773e55b
mohitaksh <59307652+mohitaksh@users.noreply.github.com>
2020-01-20 22:09:08 +0530

    Update AI Assignment.py

--- AI Assignment.py
+++ AI Assignment.py
@@ -5,13 +5,18 @@ print("c.	Good graders study well")
 print("d.	Humans love graders")
 print("e.	Every human does not study well")
 
-concerned_Data=("Every","Human","good grader","study well")
+#concerned_Data=("Every","Human","good grader","study well")
 
 print("Query : Is every human good grader")
+#Query is: is "Every" & "Human" a "Good Grader"
+#Hence Query is: {((Every) & (Human)) => Good Grader}
 
+#We have the following knowledge from the propositions -
 knowledge=[{"study well":False,"good grader":False,"human":True,"every":True},{"study well":True,"good grader":True,"human":True,"every":False},
            {"study well":False,"good grader":False,"human":True,"every":True},{"study well":False,"good grader":False,"human":False,"every":False}]
+
 p = not(knowledge[0]["human"] and knowledge[0]["every"]) or knowledge[0]["good grader"]
+
 a=1
 for i in range(len(knowledge)):
     q = not(knowledge[i]["human"] and knowledge[i]["every"]) or knowledge[i]["good grader"]
